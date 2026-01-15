da1a88ea45ba0a1ebc6934b996670cdb825500bd
Abdumajidov2005 <otkirbekabdumajidov28@gmail.com>
2026-01-15 13:12:54 +0500

    dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -4,23 +4,48 @@ import "./Dashboard.css";
 function Dashboard() {
   return (
     <>
-      <p style={{ color: "red" }}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
-        voluptatibus, temporibus quia quis vitae reprehenderit! Aliquam deserunt
-        nostrum excepturi quo tempora optio ipsam quas dignissimos, ab iure
-        odio? In, esse. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Accusantium dolorem pariatur culpa distinctio dignissimos quibusdam
-        sequi unde totam porro dicta! Delectus debitis nam qui. Praesentium
-        vitae enim saepe a quos, exercitationem, asperiores repudiandae
-        quibusdam veritatis nobis aspernatur velit laudantium alias? Adipisci
-        fugiat esse repudiandae veritatis alias minima repellat necessitatibus
-        maiores impedit facere. Alias iusto enim nisi eius exercitationem vel
-        explicabo accusamus repudiandae nesciunt, odio dolorem illum, soluta
-        quia ex ducimus deserunt rerum. Natus incidunt voluptatem voluptatum
-        dignissimos enim doloribus. Voluptate quam nulla possimus harum
-        praesentium optio, eaque nisi placeat modi nam sed ullam magnam magni
-        odio dolorum, necessitatibus nostrum cumque!
-      </p>
+      <div className="dashboard_panel">
+        <h1 className="dashboard_title">Dashboard</h1>
+        <div className="dashboard_courses">
+          <div className="dashboard_courses-card">
+            <h2>Fontend</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod.
+            </p>
+            <span>Active</span>
+            <span>2024-25-12</span>
+          </div>
+          <div className="dashboard_courses-card">
+            <h2>Fontend</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod.
+            </p>
+            <span>Active</span>
+            <span>2024-25-12</span>
+          </div>
+          <div className="dashboard_courses-card">
+            <h2>Fontend</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod.
+            </p>
+            <span>Active</span>
+            <span>2024-25-12</span>
+          </div>
+          <div className="dashboard_courses-card">
+            <h2>Fontend</h2>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde,
+              quod.
+            </p>
+            <span>Active</span>
+            <span>2024-25-12</span>
+          </div>
+        </div>
+        
+      </div>
     </>
   );
 }
